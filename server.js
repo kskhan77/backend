98c1cc15453e5912022e624ab278ae9643b7866d
khurram shafique <kshafiqu@umich.edu>
2022-03-20 17:49:12 -0400

    init

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 
 //serer production mode
 
-if (process.env.NODE_ENV === "production") {
+if ((process.env.NODE_ENV = production)) {
   app.use(express.static("../client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "bulid", "index.html"));
