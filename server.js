91ce809c1de5dc021c0166c99cbb4c62ccd05332
khurram shafique <kshafiqu@umich.edu>
2022-03-20 18:43:23 -0400

    init

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ const PORT = process.env.PORT || 5000;
 
 //serer production mode
 
-if ((process.env.NODE_ENV = production)) {
-  app.use(express.static("../client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "bulid", "index.html"));
-  });
-}
+// if ((process.env.NODE_ENV = production)) {
+//   app.use(express.static("../client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "bulid", "index.html"));
+//   });
+// }
 
 app.use(express.json());
 
